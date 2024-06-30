282ddd123a81d77a599d8a7ebd175c9c556ad2f6
chukwudumebiughonu <plasdexcel14@gmail.com>
2024-06-30 23:25:01 +0100

    modify auth page image

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -11,11 +11,11 @@ export default function RootLayout({
       <div className="auth-asset">
         <div>
           <Image 
-            src="/icons/auth-image.svg"
+            src="/icons/auth-image.png"
             alt="Auth image"
-            width={500}
-            height={500}
-            className="rounded-l-xl object-contain"
+            width={800}
+            height={800}
+            className="rounded-l-xl object-contain  border-4 border-gray-500"
           />
         </div>
       </div>
