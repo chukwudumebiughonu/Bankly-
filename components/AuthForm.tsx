d9c7b0ca394bf8bae5e9065fe473eb31c87870b1
chukwudumebiughonu <plasdexcel14@gmail.com>
2024-07-01 11:00:13 +0100

    update auth form

--- components/AuthForm.tsx
+++ components/AuthForm.tsx
@@ -125,7 +125,7 @@ const AuthForm = ({ type }: { type: string }) => {
                 <>
                   <div className="flex gap-4">
                     <CustomInput control={form.control} name='firstName' label="First Name" placeholder='Enter your first name' />
-                    <CustomInput control={form.control} name='lastName' label="Last Name" placeholder='Enter your first name' />
+                    <CustomInput control={form.control} name='lastName' label="Last Name" placeholder='Enter your last name' />
                   </div>
                   <CustomInput control={form.control} name='address1' label="Address" placeholder='Enter your specific address' />
                   <CustomInput control={form.control} name='city' label="City" placeholder='Enter your city' />
